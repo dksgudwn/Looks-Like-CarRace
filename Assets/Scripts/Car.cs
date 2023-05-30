554d840837a69882ff5d597600d78745c994e271
dksgudwn <dksgudwn620@gmail.com>
2023-05-30 16:40:57 +0900

    뭔가를 했음

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -6,56 +6,67 @@ using UnityEngine.SceneManagement;
 
 public class Car : MonoBehaviour
 {
-    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _speed;
     [SerializeField] private float _speedGainPerSecond = 0.2f;
     [SerializeField] private float turnSpeed = 200f;
 
     [SerializeField] GameObject scoreMaster;
-    [SerializeField] GameObject camera;
+    [SerializeField] GameObject _camera;
     [SerializeField] GameObject dieEffect;
-    [SerializeField] GameObject dieCanvas;
+    [SerializeField] GameObject StartBtn;
+    [SerializeField] GameObject ReStartBtn;
     float timer = 0;
 
     private float steerValue;
     Rigidbody rb;
+    private bool isMove;
 
+    private void Awake()
+    {
+        ReStartBtn.SetActive(false);
+        rb = GetComponent<Rigidbody>();
+        isMove = false;
+    }
     private void Start()
     {
-        dieCanvas.SetActive(false);
-        rb = GetComponent<Rigidbody>();
+        _speed = 0f;
     }
     void Update()
     {
-        rb.AddForce(Vector3.down * 300f, ForceMode.Acceleration);
-        _speed += _speedGainPerSecond * Time.deltaTime;
+        if (isMove)
+        {
+            rb.AddForce(Vector3.down * 10, ForceMode.Acceleration);
+            _speed += _speedGainPerSecond * Time.deltaTime;
+        }
 
         transform.Rotate(0f, steerValue * turnSpeed * Time.deltaTime, 0f);
 
         transform.Translate(Vector3.forward * _speed * Time.deltaTime);
-        Debug.Log(steerValue);
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Trigger");
         if (other.CompareTag("Obstacle"))
         {
+            Debug.Log("Obstacle");
             End();
         }
     }
 
     private void End()
     {
-        camera.GetComponent<CinemachineVirtualCamera>().m_Follow = null;
+        GetComponent<Camera>().GetComponent<CinemachineVirtualCamera>().m_Follow = null;
         gameObject.GetComponent<AudioSource>().enabled = false;
         Instantiate(dieEffect, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), Quaternion.identity);
         Handheld.Vibrate();
-        Invoke("panel", 2);
+        Invoke("GameOver", 2);
     }
 
-    private void panel()
+    private void GameOver()
     {
         scoreMaster.GetComponent<ScoreSystem>().canScoring = false;
-        dieCanvas.SetActive(true);
+        ReStartBtn.SetActive(true);
     }
 
     public void loadScene(int index)
@@ -68,8 +79,15 @@ public class Car : MonoBehaviour
         //SceneManager.LoadScene(0);
     }
 
-    public void Steer(int value) //а╤а╬го╢ы.
+    public void Steer(int value) //О©╫О©╫О©╫О©╫О©╫о╢О©╫.
     {
         steerValue = value;
+    }
+
+    public void StartBtnClick()
+    {
+        StartBtn.SetActive(false);
+        _speed = 30f;
+        isMove = true;
     }
 }
